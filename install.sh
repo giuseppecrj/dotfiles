5f290de5b7b58e010b6e3969e9c71ed9d5279820
G <giuseppecrj@gmail.com>
2022-03-16 11:49:08 +0000

    Add more git aliases

--- install.sh
+++ install.sh
@@ -50,6 +50,8 @@ git config --global user.name $full_name
 git config --global user.email $email
 git config --global init.defaultBranch main
 git config --global core.editor "code --wait"
+git config --global alias.ck "checkout"
+git config --global alias.st "status"
 # git.sh [END]
 
 # dots.sh [START]
