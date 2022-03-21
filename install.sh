a55d0b7bcecd1b46afe7c3588465f61bf2f23ea5
G <giuseppecrj@gmail.com>
2022-03-21 23:15:10 +0000

    Fix dotfiles zshrc

--- install.sh
+++ install.sh
@@ -56,7 +56,7 @@ git config --global alias.st "status"
 
 # dots.sh [START]
 
-echo "Defaulting to bash..."
+echo "Defaulting to zsh..."
 chsh -s /bin/zsh
 
 echo "Setup dotfiles..."
@@ -64,7 +64,6 @@ cd ~
 git clone git@github.com:giuseppecrj/dotfiles.git
 ln -s ~/dotfiles/env.sh ~/env.sh
 ln -s ~/dotfiles/.zshrc ~/.zshrc
-source ~/.zshrc
 
 # dots.sh [END]
 
@@ -76,7 +75,6 @@ fi
 
 echo "Installing Oh-my-zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 
 echo "Updating Homebrew Repository..."
 brew update
