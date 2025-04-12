cfe990c5d78fe2a7d09312f371debc117c65bbb7
g <5714678+giuseppecrj@users.noreply.github.com>
2025-04-12 15:26:21 -0400

    simpler env

--- env.sh
+++ env.sh
@@ -3,13 +3,12 @@ export EDITOR="cursor -w"
 export CLICOLOR=1
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 export ZSH=$HOME/.oh-my-zsh
-export PYENV_ROOT=$HOME/.pyenv
-export DOCKER_DEFAULT_PLATFORM=linux/amd64
 export GPG_TTY=$(tty)
 
 ZSH_THEME="spaceship"
 SPACESHIP_SHOW_BATTERY="false"
 SPACESHIP_GCLOUD_SHOW="false"
+SPACESHIP_PROMPT_ASYNC="false"
 FNM_USING_LOCAL_VERSION=0
 
 plugins=(git)
@@ -26,24 +25,13 @@ if test "$(which brew)"; then
     eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
-# Rbenv
-if test "$(which rbenv)"; then
-    eval "$(rbenv init -)"
-fi
-
 # fnm
 if test "$(which fnm)"; then
     eval "$(fnm env)"
 fi
 
-# pyenv
-if test "$(which pyenv)"; then
-    eval "$(pyenv init -)"
-fi
-
 # zoxide
 if test "$(which zoxide)"; then
-    source <(fzf --zsh)
     eval "$(zoxide init zsh --cmd j)"
 fi
 
@@ -52,8 +40,5 @@ source_if_exists() {
     [ -f "$1" ] && source "$1"
 }
 
-source_if_exists "$HOME/.cargo/env"
 source_if_exists "$HOME/dotfiles/hooks.sh"
-
 export PATH="$PATH:$HOME/.foundry/bin"
-export PATH="$PATH:$HOME/Library/Python/3.10/bin"
